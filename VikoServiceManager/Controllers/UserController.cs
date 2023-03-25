bfa6a98899046d110e3ed7db57117472293f985e
GiedriusMauza <giedrius.mauza@gmail.com>
2023-03-25 11:37:42 +0200

    Claims funcionality

--- VikoServiceManager/Controllers/UserController.cs
+++ VikoServiceManager/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using IdentityManager.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using VikoServiceManager.Data;
 using VikoServiceManager.Models;
 
 namespace VikoServiceManager.Controllers
@@ -42,7 +44,7 @@ namespace VikoServiceManager.Controllers
         public IActionResult Edit(string userId)
         {
             var objFromDb = _db.ApplicationUser.FirstOrDefault(u => u.Id == userId); // getting user object from database
-            if (objFromDb == null) 
+            if (objFromDb == null)
             {
                 return NotFound();
             }
@@ -59,7 +61,7 @@ namespace VikoServiceManager.Controllers
 
             objFromDb.RoleList = _db.Roles.Select(u => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem
             {
-                Text = u.Name, 
+                Text = u.Name,
                 Value = u.Id
             });
             return View(objFromDb);
@@ -67,7 +69,7 @@ namespace VikoServiceManager.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task <IActionResult> Edit(ApplicationUser user)
+        public async Task<IActionResult> Edit(ApplicationUser user)
         {
             if (ModelState.IsValid)
             {
@@ -100,8 +102,113 @@ namespace VikoServiceManager.Controllers
                 Value = u.Id
             });
             return View(user);
+        }
 
+        [HttpPost]
+        public IActionResult LockUnlock(string userId)
+        {
+            var objFromDb = _db.ApplicationUser.FirstOrDefault(u => u.Id == userId);
+            if (objFromDb == null)
+            {
+                return NotFound();
+            }
+            // checks if user lockout is not null and if it is set greater than today
+            if (objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+            {
+                // then we ulnock user 
+                objFromDb.LockoutEnd = DateTime.Now;
+                TempData[SD.Success] = "User unlocked successfully.";
+            }
+            else
+            {
+                // user is not locked and we want to lock it
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+                TempData[SD.Success] = "User locked successfully.";
+            }
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
 
+        [HttpPost]
+        public IActionResult Delete(string userId)
+        {
+            var objFromDb = _db.ApplicationUser.FirstOrDefault(u => u.Id == userId);
+            if (objFromDb == null)
+            {
+                return NotFound();
+            }
+            _db.ApplicationUser.Remove(objFromDb);
+            _db.SaveChanges();
+            TempData[SD.Success] = "User deleted successfully.";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ManageUserClaims(string userId)
+        {
+            IdentityUser user = await _userManager.FindByIdAsync(userId);
+            if (user == null) 
+            {
+                return NotFound();
+            }
+
+            var existingUserClaims = await _userManager.GetClaimsAsync(user);
+
+            // based on user id we create view model for claims
+            var model = new UserClaimsViewModel()
+            {
+                UserId = userId,
+            };
+
+            // creating user claims and adding them to our model
+            foreach (var claim in ClaimStore.claimsList)
+            {
+                UserClaim userClaim = new UserClaim()
+                {
+                    ClaimType = claim.Type,
+                };
+                if (existingUserClaims.Any(c => c.Type == claim.Type))
+                {
+                    userClaim.IsSelected = true;    
+                }
+                model.Claims.Add(userClaim);
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> ManageUserClaims(UserClaimsViewModel userClaimsViewModel)
+        {
+            IdentityUser user = await _userManager.FindByIdAsync(userClaimsViewModel.UserId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var claims = await _userManager.GetClaimsAsync(user);
+            var result = await _userManager.RemoveClaimsAsync(user, claims);
+
+            if (!result.Succeeded)
+            {
+                TempData[SD.Error] = "Error while removing claims.";
+                return View(userClaimsViewModel);
+            }
+
+            result = await _userManager.AddClaimsAsync(user, 
+                userClaimsViewModel.Claims.Where(c => c.IsSelected).Select(c => new Claim(c.ClaimType, c.IsSelected.ToString()))
+                );
+
+            if (!result.Succeeded)
+            {
+                TempData[SD.Error] = "Error while removing claims.";
+                return View(userClaimsViewModel);
+            }
+
+            TempData[SD.Success] = "Claims updated successfully.";
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ VikoServiceManager/Models/UserClaimsViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace VikoServiceManager.Models
+{
+    public class UserClaimsViewModel
+    {
+        public string UserId { get; set; }
+        public List<UserClaim> Claims { get; set; }
+
+        public UserClaimsViewModel()
+        {
+            Claims = new List<UserClaim>();
+        }
+    }
+
+    public class UserClaim
+    {
+        public string ClaimType { get; set; }
+        public bool IsSelected { get; set;}
+    }
+}
